4d0e07f2b040e0a0fe0e61a0a957d973e2070d68
lalit.tanwar <lalit.tanwar07@gmail.com>
2021-11-13 07:05:41 +0530

    added time

--- src/trade/History.vue
+++ src/trade/History.vue
@@ -24,6 +24,11 @@
             {{(trade.item.quantity * trade.item.price) | round2}}
         </template>
 
+        <template #cell(time)="trade">
+            <span v-if="trade.item.timestamp>day">24h</span>
+            <span v-else-if="trade.item.timestamp>week">Wk</span>
+        </template>
+
        </b-table>
       </a-card>
 
@@ -39,12 +44,15 @@ export default {
       myTrades : {},
     },
     data: () => ({
+        day : Date.now()-1000*60*60*24,
+        week : Date.now()-1000*60*60*24*7,
         fields : [
             { key: 'symbol', label : "Coin"},
             { key: 'side', label : "Side"},
             { key: 'price', label : "Price"},
             { key: 'quantity', label : "TQty"},
-            { key: 'amount', label : "Amount"}
+            { key: 'amount', label : "Amount"},
+            {key : 'time' , label : "When"}
         ]
   }),
 }
