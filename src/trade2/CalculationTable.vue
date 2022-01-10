4ecd5ad2d45f1903983a98f379fc90702a60bec9
amittnt <84276312+amittnt@users.noreply.github.com>
2022-01-10 22:09:47 +0530

    1

--- src/trade2/CalculationTable.vue
+++ src/trade2/CalculationTable.vue
@@ -350,14 +350,14 @@ table{ /*or other element you want*/
    	background-color: #a30c0c;
     color: white!important;
     border-radius: 0px;
-    padding-inline: 5px;
+    padding: 4px;
 
    }
    .tbb{
    	background-color: #128961;
     color: white!important;
     border-radius: 0px;
-    padding-inline: 5px;
+    padding: 5px;
 
 
    }
